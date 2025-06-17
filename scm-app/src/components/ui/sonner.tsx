0adf1689097f3281b8a320d9e54b83fd7c567e02
caizhen <zhenchai0000@gmail.com>
2025-06-17 16:02:37 +0800

    update: shadcn ui component

--- scm-app/src/components/ui/sonner.tsx
+++ scm-app/src/components/ui/sonner.tsx
@@ -10,6 +10,10 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      position="top-center"
+      expand={true}
+      richColors={true}
+      closeButton={true}
       style={
         {
           "--normal-bg": "var(--popover)",
