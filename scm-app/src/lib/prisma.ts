faa1679eebda446f2230fe03b6212d07416466cb
caizhen <zhenchai0000@gmail.com>
2025-06-17 16:03:47 +0800

    fix: prisma too many client error
    - update by changing the prisma instance to singleton

--- scm-app/src/lib/prisma.ts
+++ scm-app/src/lib/prisma.ts
@@ -3,6 +3,18 @@
 
 import { PrismaClient } from "generated/prisma";
 
-const prisma = new PrismaClient();
+declare global {
+    var globalPrisma: PrismaClient | undefined;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+} else {
+    if (!global.globalPrisma) {
+        global.globalPrisma = new PrismaClient();
+    }
+    prisma = global.globalPrisma;
+}
 
 export default prisma;
